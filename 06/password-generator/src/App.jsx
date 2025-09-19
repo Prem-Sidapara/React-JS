210bf8807248831300fce2e0544ec37519b4e6c2
Prem Sidapara <pprem1644@gmail.com>
2025-09-19 23:48:52 +0530

    color updates

--- 06/password-generator/src/App.jsx
+++ 06/password-generator/src/App.jsx
@@ -28,13 +28,13 @@ const App=()=> {
   }
 
   return (
-    <div className="min-h-screen bg-black/90 flex items-center justify-center">
-      <div className="flex flex-col items-center gap-3 text-4xl p-16 w-lg rounded-lg bg-white/70  hover:scale-101 duration-400 transition-transform font-bold">
+    <div className="h-screen bg-[conic-gradient(from_355deg,_#30cfd0_0%_50%,_#330867_50%_100%)] flex items-center justify-center">
+      <div className="flex flex-col items-center gap-3 text-4xl p-16 w-lg rounded-lg bg-[conic-gradient(from_355deg,_#d4fc79_0%_50%,_#96e6a1_50%_100%)]  hover:scale-101 duration-400 transition-transform font-bold">
         <LockKeyhole className='text-black/100 w-12 h-12' /> 
         <h1 className='text-3xl font-bold '>Password Generator</h1>
-        <form className='w-full  rounded-lg bg-black/90 mt-6 p-5 flex flex-col gap-4'  onSubmit={generatePassword} >
+        <form className='w-full  rounded-lg bg-[conic-gradient(from_355deg,_#a1c4fd_0%_50%,_#c2e9fb_50%_100%)] mt-6 p-5 flex flex-col gap-4'  onSubmit={generatePassword} >
           <input input='number' placeholder="Enter Password Length" className=" bg-white/70 text-black border-2 border-black/70 rounded-lg p-2 text-block/100 w-full focus:outline-none focus:border-black/100 focus:bg-white/100 focus:ring-1 focus:ring-black/100 text-lg font-semibold" />
-          <button className=' flex items-center gap-2 bg-black/90 text-white border-2 border-white/70 rounded-lg p-2 text-white/100 w-full hover:bg-green-500 hover:border-black/100 hover:text-white/100 focus:outline-none focus:ring-1 focus:ring-black/100 text-lg justify-center font-semibold '>
+          <button className=' flex items-center gap-2 bg-green-500 text-white border-1 border-black/70 rounded-lg p-2 text-white/100 w-full hover:bg-green-500 hover:border-2 hover:border-black/100 hover:text-white/100 focus:outline-none focus:ring-1 focus:ring-black/100 text-lg justify-center font-semibold '>
             <KeyRound className='text-white' />
             Generate Strong Password
           </button>
@@ -42,8 +42,8 @@ const App=()=> {
         {
           password !== '' && 
         
-        <div className='w-full bg-black/90 text-white border-2 border-white/70 rounded-lg p-4 text-white/100 flex justify-between items-center text-lg font-semibold mt-4'>
-          <p>{password}</p>
+        <div className='w-full bg-black/90 max-h-20 text-white border-2 border-white/70 rounded-lg p-4 text-white/100 flex justify-between items-center text-lg font-semibold mt-4'>
+          <p className=' w-80 truncate'>{password}</p>
           <Copy className='text-white hover:scale-110 transition-transform duration-100' onClick={copyPassword} />
         </div>
       }       
