b0da194308a8137be3e9cebb8fce4eb7880574fc
Prem Sidapara <prem.sidapara.main@gmail.com>
2025-09-27 00:35:26 +0530

    Update App.jsx

--- 07/currency-convertor/src/App.jsx
+++ 07/currency-convertor/src/App.jsx
@@ -44,14 +44,14 @@ const App=()=> {
     
       <div className='w-full '>
         <div className='w-full max-w-md mx-auto border border-gray-60 rounded-xl p-6 backdrop-blur-sm bg-white/30 shadow-2xl'>
-          <h1 className="text-center text-white text-3xl font-bold mb-6 tracking-wider">Currency Converter</h1>
+          <h1 className="text-center text-white-300 text-3xl font-bold mb-6 tracking-wider">Currency Converter</h1>
           <form 
           className='flex flex-col gap-4'
             onSubmit={(e)=>{
             e.preventDefault();
             convert();
           }}>
-            <div className='w-full mb-1'>
+            <div className='w-full '>
               <InputBox
               label="From"
               amount={amount}
@@ -62,9 +62,9 @@ const App=()=> {
               />
             </div>
 
-            <div  className='relative w-full h-0.5'>
+            <div  className='relative w-full '>
               <button 
-                className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-lg bg-blue-600 hover:bg-blue-700 transition-colors text-white px-3 py-1 font-semibold shadow-md'
+                className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white/50 rounded-lg bg-blue-600 hover:bg-blue-700 transition-colors text-white px-3 py-1 font-semibold shadow-md'
               onClick={swap}
               type="button"
               >
